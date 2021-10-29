768b93bfa3fbc45c896ef03b699514c7944342a6
winters0727 <winters0727@gmail.com>
2021-10-29 11:42:14 +0900

    chore: eslint 및 prettier 설정

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,9 @@ const app: express.Application = express();
 
 // get
 app.get("/", (req: express.Request, res: express.Response) => {
-    res.send("hello express");
+  res.send("hello express");
 });
 
 app.listen(3000, () => {
-    console.log("Running Server port 3000");
+  console.log("Running Server port 3000");
 });
