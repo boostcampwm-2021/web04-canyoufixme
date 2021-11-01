17ec90359bcfa9432321651c05d03abb40938ede
winters0727 <winters0727@gmail.com>
2021-11-01 17:25:31 +0900

    feat: 라우터 설정

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="Navbar"></div>
       </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/debug" exact />
+          <Redirect path="*" to="/notfound" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
