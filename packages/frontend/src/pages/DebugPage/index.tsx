709068fb70b4a162e065b0fbf38ce823ad24f2ed
Xvezda <xvezda@naver.com>
2021-11-03 14:57:39 +0900

    feat: 기본 디버깅 로직 구현 및 에디터 초기화 버그 수정

--- packages/frontend/src/pages/DebugPage/index.tsx
+++ packages/frontend/src/pages/DebugPage/index.tsx
@@ -1,16 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useMemo, useCallback } from 'react';
 import type { MutableRefObject, RefObject } from 'react';
 
 import AceEditor from 'react-ace';
 import { Viewer } from '@toast-ui/react-editor';
 
+import runner from './debug';
 import styled from '@cyfm/styled';
 
 import 'ace-builds/src-noconflict/mode-javascript';
-import 'ace-builds/src-noconflict/theme-xcode';
+import 'ace-builds/src-noconflict/theme-twilight';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
-import Editor from '@toast-ui/editor';
 
 class FullWidthViewer extends Viewer {
   componentDidMount(this: { rootEl: RefObject<HTMLElement> }) {
@@ -20,73 +20,89 @@ class FullWidthViewer extends Viewer {
   }
 }
 
+const FlexWrapper = styled.div`
+  display: flex;
+  min-width: 100vw;
+  min-height: 100vh;
+`;
+
+const FlexColumnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  width: 50%;
+`;
+
+const ViewerWrapper = styled.div`
+  display: flex;
+  flex-basis: 50%;
+  width: 100%;
+  overflow-y: auto;
+  box-sizing: border-box;
+`;
+
+const EditorWrapper = styled.div`
+  display: flex;
+  flex-basis: 50%;
+  box-sizing: border-box;
+`;
+
+const ConsoleWrapper = styled.div`
+  padding: 20px;
+  height: 100%;
+  flex-basis: 50%;
+  background: #24262a;
+  color: white;
+  box-sizing: border-box;
+`;
+
+const Button = styled.button`
+  padding: 0.5em 0.7em;
+  border: 0;
+  font-size: 1.2em;
+  background-color: #f6cc00;
+`;
+
+const ButtonFooter = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  background: #1c1d20;
+`;
 
 const DebugPage: React.FC = () => {
+  const [output, setOutput] = useState('');
+
+  const initialProblem = `function getTypeName(arrayOrObject) {
+    if (typeof arrayOrObject === 'object') {
+      return 'object';
+    } else {
+      return 'array';
+    }
+  }`;
+
   const viewerRef: MutableRefObject<Viewer | undefined> = useRef();
   const editorRef: MutableRefObject<AceEditor | string | undefined> = useRef();
 
-  function onChange(newValue: string) {
-    editorRef.current = newValue;
-  }
+  const onChange = useCallback(
+    (newValue: string) => {
+      editorRef.current = newValue;
+    },
+    [editorRef],
+  );
 
-  function onClick() {
-    viewerRef.current?.getInstance().setMarkdown('### 마크다운 예시');
-  }
+  const onLoad = useCallback(editor => {
+    onChange(editor.getValue());
+  }, []);
 
   function getValue() {
     return editorRef.current;
   }
 
-  function onExecute() {
-    const func = new Function(getValue() as string);
-    func();
-  }
-
-  const FlexWrapper = styled.div`
-    display: flex;
-    min-width: 100%;
-  `;
-
-  const FlexColumnWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 50%;
-  `;
-
-  const ViewerWrapper = styled.div`
-    display: flex;
-    flex-basis: 50%;
-    width: 50%;
-    overflow-y: auto;
-    padding: 1em;
-  `;
-
-  class FullWidthViewer extends Viewer {
-    componentDidMount() {
-      Viewer.prototype.componentDidMount?.call(this);
+  const onExecute = useCallback(() => {
+    if (runner(getValue() as string, setOutput)) {
+      setOutput('축하합니다. 멋지게 해내셨네요! 🥳');
     }
-  }
-
-  const ConsoleWrapper = styled.div`
-    display: inline-block;
-  `;
-
-  const EditorWrapper = styled.div`
-    display: flex;
-    flex-basis: 50%;
-  `;
-
-  const Button = styled.button`
-    padding: 0.8em 1.2em;
-    font-size: 1.2em;
-    background-color: yellow;
-    border-radius: 15px;
-  `;
-
-  const ButtonFooter = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-  `;
+  }, [setOutput]);
 
   return (
     <FlexWrapper>
@@ -105,15 +121,24 @@ const DebugPage: React.FC = () => {
       <FlexColumnWrapper>
         <EditorWrapper>
           <AceEditor
+            onLoad={onLoad}
             onChange={onChange}
             mode="javascript"
-            theme="xcode"
+            width="100%"
+            height="800px"
+            theme="twilight"
             name="test"
+            fontSize={16}
+            defaultValue={initialProblem}
             editorProps={{ $blockScrolling: true }}
+            setOptions={{
+              tabSize: 2,
+              enableBasicAutocompletion: true,
+              enableLiveAutocompletion: true,
+            }}
           />
         </EditorWrapper>
         <ButtonFooter>
-          <Button onClick={onClick}>출력</Button>
           <Button onClick={onExecute}>실행</Button>
         </ButtonFooter>
       </FlexColumnWrapper>
