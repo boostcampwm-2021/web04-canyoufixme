d34a9b301ea7ae709aa95fd87008a28dbda5496e
Xvezda <xvezda@naver.com>
2021-11-03 14:25:54 +0900

    feat: 디버깅 페이지 에디터 컴포넌트 추가
    - 마크다운 에디터 추가
    - 코드 에디터 추가

--- packages/frontend/src/pages/DebugPage/index.tsx
+++ packages/frontend/src/pages/DebugPage/index.tsx
@@ -1,11 +1,15 @@
 import React, { useRef } from 'react';
 import AceEditor from 'react-ace';
-import type { MutableRefObject } from 'react';
+import { Editor, Viewer } from '@toast-ui/react-editor';
+import type { MutableRefObject, RefObject } from 'react';
 
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/theme-xcode';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 const DebugPage: React.FC = () => {
+  const markdownRef: MutableRefObject<Editor | undefined> = useRef();
+  const viewerRef: MutableRefObject<Viewer | undefined> = useRef();
   const editorRef: MutableRefObject<AceEditor | string | undefined> = useRef();
 
   function onChange(newValue: string) {
@@ -13,16 +17,43 @@ const DebugPage: React.FC = () => {
   }
 
   function onClick() {
-    console.log(editorRef.current);
+    const markdown = markdownRef.current?.getInstance().getMarkdown() as string;
+    viewerRef.current?.getInstance().setMarkdown(markdown);
+  }
+
+  function getValue() {
+    return editorRef.current;
   }
 
   function onExecute() {
-    const func = new Function(editorRef.current as string);
+    const func = new Function(getValue() as string);
     func();
+  }
+
+  function onMarkdownEditorLoad(this: Editor) {
+    markdownRef.current = this;
   }
 
   return (
     <div>
+      <Editor
+        previewStyle="vertical"
+        height="400px"
+        initialEditType="wysiwyg"
+        initialValue="### 마크다운 문법에 맞춰 값을 입력해주세요."
+        theme="dark"
+        onLoad={onMarkdownEditorLoad}
+        ref={markdownRef as RefObject<Editor>}
+      />
+      <div
+        style={{
+          width: '200px',
+          maxHeight: '300px',
+          overflowY: 'auto',
+        }}
+      >
+        <Viewer ref={viewerRef as RefObject<Viewer>} />
+      </div>
       <AceEditor
         onChange={onChange}
         mode="javascript"
