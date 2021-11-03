70884411f078df18f3d46e694c5b8e00db153fe1
Xvezda <xvezda@naver.com>
2021-11-03 18:08:22 +0900

    feat: PORT 환경변수 설정 기능 및 CORS 미들웨어 구현

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,21 @@ import * as express from 'express';
 
 const app: express.Application = express();
 
-// get
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('hello express');
+app.use(
+  (
+    req: express.Request,
+    res: express.Response,
+    next: (reason?: Error) => void,
+  ) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    next();
+  },
+);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  /* eslint-disable-next-line no-console */
+  console.log(`Running Server port ${port}`);
 });
 
 app.listen(3000, () => {
