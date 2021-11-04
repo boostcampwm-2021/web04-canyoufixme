6d1f8a6b7e38f863a97589f60640c07f86a05361
Xvezda <xvezda@naver.com>
2021-11-04 17:04:56 +0900

    chore: 문제 리스트 페이지 레이아웃 정렬 방식 변경

--- packages/frontend/src/pages/ListPage/index.tsx
+++ packages/frontend/src/pages/ListPage/index.tsx
@@ -9,7 +9,7 @@ const Background = styled.div`
 const ListWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
+  justify-content: flex-start;
   align-items: center;
   width: 100%;
   height: 100%;
