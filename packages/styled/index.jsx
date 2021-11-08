115e9ec83a5790ac4f0994b10cd439da657aba91
winters0727 <winters0727@gmail.com>
2021-11-08 19:01:14 +0900

    feat: Callback props 기능 지원
    Related #53
    
    Co-authored-by: Xvezda <xvezda@naver.com>

--- packages/styled/index.jsx
+++ packages/styled/index.jsx
@@ -1,90 +1,26 @@
 import React from 'react';
+/* eslint-disable-next-line import/extensions,import/no-unresolved */
+import tags from './tags';
 
-export const tags = [
-  'a',
-  'abbr',
-  'address',
-  'article',
-  'aside',
-  'audio',
-  'b',
-  'bdo',
-  'bdi',
-  'blockquote',
-  'br',
-  'button',
-  'canvas',
-  'cite',
-  'code',
-  'command',
-  'data',
-  'datalist',
-  'del',
-  'details',
-  'dfn',
-  'div',
-  'dl',
-  'em',
-  'embed',
-  'fieldset',
-  'figure',
-  'footer',
-  'form',
-  'h1',
-  'h2',
-  'h3',
-  'h4',
-  'h5',
-  'h6',
-  'header',
-  'hgroup',
-  'hr',
-  'i',
-  'iframe',
-  'img',
-  'input',
-  'ins',
-  'kbd',
-  'keygen',
-  'label',
-  'main',
-  'map',
-  'mark',
-  'math',
-  'menu',
-  'meter',
-  'nav',
-  'noscript',
-  'object',
-  'ol',
-  'output',
-  'p',
-  'picture',
-  'pre',
-  'progress',
-  'q',
-  'ruby',
-  's',
-  'samp',
-  'script',
-  'section',
-  'select',
-  'small',
-  'span',
-  'strong',
-  'sub',
-  'sup',
-  'svg',
-  'table',
-  'template',
-  'textarea',
-  'time',
-  'u',
-  'ul',
-  'var',
-  'video',
-  'wbr',
-];
+function parseCss(css) {
+  return Object.fromEntries(
+    css
+      .split(';')
+      .filter(x => x)
+      .map(s => s.split(':').map(v => v.trim()))
+      .map(([k, v]) => [
+        k.replace(/-([a-z])/g, (_, p1) => p1.toUpperCase()),
+        v,
+      ]),
+  );
+}
+
+function processTemplate(strings, args, props) {
+  return strings.slice(1).reduce((acc, v, i) => {
+    const arg = args[i];
+    return acc + (typeof arg === 'function' ? arg(props) : arg) + v;
+  }, strings[0]);
+}
 
 const styled = tags.reduce((acc, tag) => {
   return {
@@ -93,21 +29,12 @@ const styled = tags.reduce((acc, tag) => {
     [tag]: (strings, ...args) => {
       /* eslint-disable-next-line react/prop-types */
       return props => {
+        const css = processTemplate(strings, args, props);
         return React.createElement(
           tag,
           {
             ...props,
-            style: Object.fromEntries(
-              strings
-                .join('')
-                .split(';')
-                .filter(x => x)
-                .map(s => s.split(':').map(v => v.trim()))
-                .map(([k, v]) => [
-                  k.replace(/-([a-z])/g, (match, p1) => p1.toUpperCase()),
-                  v,
-                ]),
-            ),
+            style: parseCss(css),
           },
           /* eslint-disable-next-line react/prop-types, react/destructuring-assignment */
           props.children,
