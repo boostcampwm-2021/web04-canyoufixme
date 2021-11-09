f514dae1a5777aa7453ce3d7648a7578cfbf1373
naknak <cnh6123@naver.com>
2021-11-09 21:28:17 +0900

    fix: mongodb 데이터 타입에서 _id 속성 제거

--- packages/backend/src/model/ProblemCode.ts
+++ packages/backend/src/model/ProblemCode.ts
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 export const ProblemCode = new Schema({
-  _id: String,
   code: String,
   testCode: [String],
   content: String,
--- packages/backend/src/model/SubmitCode.ts
+++ packages/backend/src/model/SubmitCode.ts
@@ -3,6 +3,5 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 export const SubmitCode = new Schema({
-  _id: Number,
   code: String,
 });
