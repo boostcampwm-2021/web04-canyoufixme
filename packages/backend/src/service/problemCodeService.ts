474234fcdff03e7b630a7a5b9d4068a51d19ab7a
naknak <cnh6123@naver.com>
2021-11-09 21:29:39 +0900

    feat: 문제와 문제에 대한 코드 조회 비즈니스 로직 작성(service 구현)

---
+++ packages/backend/src/service/problemCodeService.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prefer-destructuring */
+import express from 'express';
+import { ProblemCodeModel } from '../../settings/mongoConfig';
+
+const findProblemById = async (req: express.Request, res: express.Response) => {
+  const id = req.params.id;
+  const problemCode = await ProblemCodeModel.findOne({ _id: id });
+  return res.json(problemCode);
+};
+
+export { findProblemById };
