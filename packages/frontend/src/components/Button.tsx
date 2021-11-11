0db005c686e640ca3e9880cf18b68aa7a345208c
winters0727 <winters0727@gmail.com>
2021-11-11 21:57:55 +0900

    refactor: Editor와 Viewer 배치 및 조절 코드 분리
    Co-authored-by: Xvezda <xvezda@naver.com>

---
+++ packages/frontend/src/components/Button.tsx
@@ -0,0 +1,10 @@
+import styled from '@cyfm/styled';
+
+const Button = styled.button`
+  padding: 0.5em 0.7em;
+  border: 0;
+  font-size: 1.2em;
+  background-color: #f6cc00;
+`;
+
+export default Button;
