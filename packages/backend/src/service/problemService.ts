9ba3ec070c1437ac477adf42f6693ba3bb4e6a61
naknak <cnh6123@naver.com>
2021-11-11 16:33:46 +0900

    feat: Mysql에 문제 데이터 저장
    문제 데이터(제목, 출제자, 난이도, 언어, 코드 MongoDB ID)를 Mysql에 저장

--- packages/backend/src/service/problemService.ts
+++ packages/backend/src/service/problemService.ts
@@ -3,6 +3,7 @@
 import express from 'express';
 import { Problem } from '../model/Problem';
 import { ProblemCodeModel } from '../../settings/mongoConfig';
+import { User } from '../model/User';
 
 const getList = async (req: express.Request, res: express.Response) => {
   const problems = await Problem.find({ relations: ['author'] });
@@ -20,3 +21,14 @@ const saveCode = async (code, content, testCode) => {
 };
 
 const getCodeId = codeData => codeData['_id'].toString();
+
+const saveProblem = async ({ title, category, level, author, codeId }) => {
+  const problem = new Problem();
+  problem.title = title;
+  problem.category = category;
+  problem.level = level;
+  problem.author = author;
+  problem.codeId = codeId;
+
+  await problem.save();
+};
