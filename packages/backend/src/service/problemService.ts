8abe156d67080dbd93639fe65032e14bf4ac25f3
naknak <cnh6123@naver.com>
2021-11-11 16:26:11 +0900

    chore: problemService 내의 return 문 제거

--- packages/backend/src/service/problemService.ts
+++ packages/backend/src/service/problemService.ts
@@ -1,9 +1,11 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable dot-notation */
 import express from 'express';
 import { Problem } from '../model/Problem';
 
 const getList = async (req: express.Request, res: express.Response) => {
   const problems = await Problem.find({ relations: ['author'] });
-  return res.json(problems);
+  res.json(problems);
 };
 
 export { getList };
