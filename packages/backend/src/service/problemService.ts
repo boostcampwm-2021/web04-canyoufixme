a2e495d7885ee77a77e7b1da8f21f5400bb17c9f
naknak <cnh6123@naver.com>
2021-11-11 16:31:27 +0900

    feat: MongoDB에 문제에 대한 코드 저장 및 id 추출
    문제에 대한 코드, 문제 설명, 테스트 코드 저장 및 해당 Object id 추출 함수 분리

--- packages/backend/src/service/problemService.ts
+++ packages/backend/src/service/problemService.ts
@@ -2,10 +2,21 @@
 /* eslint-disable dot-notation */
 import express from 'express';
 import { Problem } from '../model/Problem';
+import { ProblemCodeModel } from '../../settings/mongoConfig';
 
 const getList = async (req: express.Request, res: express.Response) => {
   const problems = await Problem.find({ relations: ['author'] });
   res.json(problems);
 };
 
-export { getList };
+const saveCode = async (code, content, testCode) => {
+  const problemCode = new ProblemCodeModel();
+  problemCode.code = code;
+  problemCode.content = content;
+  problemCode.testCode = testCode;
+
+  const codeData = await problemCode.save();
+  return codeData;
+};
+
+const getCodeId = codeData => codeData['_id'].toString();
