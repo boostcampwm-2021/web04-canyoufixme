077f49ce8eb8b37d022acb6567428d92016069ab
naknak <cnh6123@naver.com>
2021-11-15 11:52:34 +0900

    chore: ormconfig entities 배열 값을 import 객체 값으로 수정

--- packages/backend/src/settings/ormConfig.ts
+++ packages/backend/src/settings/ormConfig.ts
@@ -1,6 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { ConnectionOptions } from 'typeorm';
 
+import { User } from '../model/User';
+import { Problem } from '../model/Problem';
+import { SubmitLog } from '../model/SubmitLog';
+
 const ormConfig: ConnectionOptions = {
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
@@ -10,6 +14,6 @@ const ormConfig: ConnectionOptions = {
   database: process.env.MYSQL_DATABASE,
   synchronize: true,
   logging: false,
-  entities: ['src/model/**/*.ts'],
+  entities: [User, Problem, SubmitLog],
 };
 export default ormConfig;
