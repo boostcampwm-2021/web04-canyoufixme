61738f86dcf035f5fc11c363960070664a2765f0
winters0727 <winters0727@gmail.com>
2021-11-17 16:54:13 +0900

    feat: 기본 Modal 창 및 버튼 레이아웃 생성

---
+++ packages/frontend/src/components/Modal/Modal.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+interface ModalProps {
+  isOpen: boolean;
+  setter: (isOpen: boolean) => void;
+  children: React.ReactNode;
+}
+
+const CustomModal = (props: ModalProps) => {
+  const { isOpen, setter, children } = props;
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={() => setter(false)}
+      style={{
+        overlay: {
+          position: 'fixed',
+          background: 'rgba(0,0,0,0.5)',
+        },
+        content: {
+          display: 'flex',
+          margin: 'auto',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '400px',
+          height: '200px',
+          borderRadius: '15px',
+          backgroundColor: '#f6cb01',
+          boxShadow: '0 0 0 6px black, 0 0 0 12px #f6cb01',
+          boxSizing: 'border-box',
+          fontSize: '1.5em',
+        },
+      }}
+    >
+      {children}
+    </Modal>
+  );
+};
+
+export default CustomModal;
