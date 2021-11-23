2aaf8521371bc2e5645fd7a2f38bdf2ce87617b2
naknak <cnh6123@naver.com>
2021-11-23 16:45:25 +0900

    fix: debugService 내의 runner에 process 관련 메소드를 막도록 설정

--- packages/backend/src/service/debugService.ts
+++ packages/backend/src/service/debugService.ts
@@ -4,6 +4,10 @@
 const runner = ({ chaiString, code, testCode }) => {
   const codeRunner = new Function(`
     ${chaiString}
+    //프로세스 조작 시스템 명령어 방지용 코드
+    const process = undefined;
+    Object.freeze(process);
+
     const { expect } = chai;
     ${code}
     ${testCode}
