279b903bb985b8ec297dadba8edbf47d2f0f3d9d
naknak <cnh6123@naver.com>
2021-11-25 16:12:49 +0900

    feat: 전체 회원 수 조회에 대한 API 컨트롤러생성

--- packages/backend/src/controller/statisticController.ts
+++ packages/backend/src/controller/statisticController.ts
@@ -1,10 +1,15 @@
 /* eslint-disable import/namespace */
 import express from 'express';
-import { getAllProblemNum, getAllSubmitNum } from '../service/statisticService';
+import {
+  getAllProblemNum,
+  getAllSubmitNum,
+  getUserCount,
+} from '../service/statisticService';
 
 const router = express.Router();
 
 router.get('/problems', getAllProblemNum);
 router.get('/submits', getAllSubmitNum);
+router.get('/users', getUserCount);
 
 export { router };
