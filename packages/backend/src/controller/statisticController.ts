437da450255d9f1327e53703d49d2e6ec9c44275
naknak <cnh6123@naver.com>
2021-11-25 16:02:41 +0900

    feat: 전체 문제 제출 횟수 조회에 대한 API 컨트롤러생성

--- packages/backend/src/controller/statisticController.ts
+++ packages/backend/src/controller/statisticController.ts
@@ -1,8 +1,10 @@
+/* eslint-disable import/namespace */
 import express from 'express';
-import { getAllProblemNum } from '../service/statisticService';
+import { getAllProblemNum, getAllSubmitNum } from '../service/statisticService';
 
 const router = express.Router();
 
 router.get('/problems', getAllProblemNum);
+router.get('/submits', getAllSubmitNum);
 
 export { router };
