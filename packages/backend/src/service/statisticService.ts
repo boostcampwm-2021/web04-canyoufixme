a220d3afa1f83ba18ca06064e61bc34f43944719
naknak <cnh6123@naver.com>
2021-11-25 16:19:07 +0900

    feat: 제출 수가 가장 많은 문제 top 5에 대한 API 로직 함수 추가

--- packages/backend/src/service/statisticService.ts
+++ packages/backend/src/service/statisticService.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable dot-notation */
 import express from 'express';
+import { getRepository } from 'typeorm';
 import { Problem } from '../model/Problem';
 import { SubmitLog } from '../model/SubmitLog';
 import { User } from '../model/User';
@@ -42,4 +43,26 @@ const getUserCount = async (req: express.Request, res: express.Response) => {
     });
   }
 };
-export { getAllProblemNum, getAllSubmitNum, getUserCount };
+const mostSubmitProblems = async (
+  req: express.Request,
+  res: express.Response,
+) => {
+  try {
+    const submitProblems = await getRepository(SubmitLog)
+      .createQueryBuilder('submitlog')
+      .select('COUNT(submitlog.problem)', 'count')
+      .leftJoinAndSelect('submitlog.problem', 'problem')
+      .skip(0)
+      .take(5)
+      .groupBy('submitlog.problem')
+      .orderBy('count', 'DESC')
+      .getRawMany();
+    res.status(200).json(submitProblems);
+  } catch (err) {
+    res.status(500).json({
+      message: 'can not get most submit problems',
+      error: err.message,
+    });
+  }
+};
+export { getAllProblemNum, getAllSubmitNum, getUserCount, mostSubmitProblems };
