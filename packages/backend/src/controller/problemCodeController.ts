46b54ff114c029493e25948dd993b651dfe6c526
naknak <cnh6123@naver.com>
2021-11-30 21:58:52 +0900

    refactor: problemCodeController와 problemController 통합

--- packages/backend/src/controller/problemCodeController.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { findProblemById } from '../service/problemCodeService';
-
-const router = express.Router();
-
-router.get('/:id', findProblemById);
-
-export { router };
--- packages/backend/src/controller/problemController.ts
+++ packages/backend/src/controller/problemController.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import { writeProblem } from '../service/problemService';
+import { findProblemCodeById } from '../service/problemCodeService';
 
-import { isLogin } from '../service/loginService';
+import { isLogin } from '../middlewares/isLogin';
 
 const router = express.Router();
 
+router.get('/:id', findProblemCodeById);
 router.post('/', isLogin, writeProblem);
 
 export { router };
