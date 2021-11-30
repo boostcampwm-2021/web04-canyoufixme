10ea3ce540d861fea5bea7bb3a4b3a36702d745e
naknak <cnh6123@naver.com>
2021-11-30 21:51:25 +0900

    refactor: 함수 이름 변경 및 response status code 추가

--- packages/backend/src/service/problemCodeService.ts
+++ packages/backend/src/service/problemCodeService.ts
@@ -2,10 +2,13 @@
 import express from 'express';
 import { ProblemCodeModel } from '../settings/mongoConfig';
 
-const findProblemById = async (req: express.Request, res: express.Response) => {
+const findProblemCodeById = async (
+  req: express.Request,
+  res: express.Response,
+) => {
   const id = req.params.id;
   const problemCode = await ProblemCodeModel.findOne({ _id: id });
-  return res.json(problemCode);
+  return res.status(200).json(problemCode);
 };
 
-export { findProblemById };
+export { findProblemCodeById };
