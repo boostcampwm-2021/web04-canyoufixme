982405dc2f6c343c1545917db09083be59ed3780
Xvezda <xvezda@naver.com>
2021-12-02 15:08:42 +0900

    feat: 타임아웃 이벤트 및 옵션 추가

--- packages/frontend/src/hooks/useSandbox/index.ts
+++ packages/frontend/src/hooks/useSandbox/index.ts
@@ -13,6 +13,7 @@ interface ISandboxHookOptions extends ISandboxOptions {
   onInit?: (event: CustomEvent) => void;
   onExit?: (event: CustomEvent) => void;
   onError?: (event: CustomEvent) => void;
+  onTimeout?: (event: CustomEvent) => void;
   onLoadStart?: (event: CustomEvent) => void;
   onLoadEnd?: (event: CustomEvent) => void;
   timeout?: number;
@@ -34,6 +35,7 @@ export function useSandbox(
     onInit: () => {},
     onExit: () => {},
     onError: () => {},
+    onTimeout: () => {},
     onLoadStart: () => {},
     onLoadEnd: () => {},
     timeout: 0,
@@ -79,9 +81,6 @@ export function useSandbox(
     const onExit = (options.onExit ??
       (event => {
         const customEvent = event as CustomEvent;
-        if (parseInt(customEvent.detail, 10) === SIGKILL) {
-          console.log(`TimeoutError: timeout`);
-        }
         console.log(`[EXIT CODE: ${customEvent.detail || -1}]`);
 
         // 재시작
@@ -105,6 +104,12 @@ export function useSandbox(
     const onError = (options.onError ?? (() => {})) as EventListener;
     sandboxRef.current.addEventListener('error', onError);
 
+    const onTimeout = (options.onTimeout ??
+      (() => {
+        console.error(`TimeoutError: timeout`);
+      })) as EventListener;
+    sandboxRef.current.addEventListener('timeout', onTimeout);
+
     let timeoutTimer: NodeJS.Timer | number = INIT_TIMER_ID;
     let delayTimer: NodeJS.Timer | number = INIT_TIMER_ID;
     const onExec = (event => {
@@ -112,6 +117,7 @@ export function useSandbox(
         clearTimeout(timeoutTimer as number);
         timeoutTimer = INIT_TIMER_ID;
         timeoutTimer = setTimeout(() => {
+          sandboxRef.current?.dispatchEvent(new CustomEvent('timeout'));
           sandboxRef.current?.dispatchEvent(
             new CustomEvent('kill', { detail: SIGKILL }),
           );
@@ -141,6 +147,7 @@ export function useSandbox(
       sandboxRef.current?.removeEventListener('init', onInit);
       sandboxRef.current?.removeEventListener('exit', onExit);
       sandboxRef.current?.removeEventListener('error', onError);
+      sandboxRef.current?.removeEventListener('timeout', onTimeout);
       sandboxRef.current?.removeEventListener('stdout', onOutput);
       sandboxRef.current?.removeEventListener('stderr', onOutputError);
       sandboxRef.current?.removeEventListener('exec', onExec);
